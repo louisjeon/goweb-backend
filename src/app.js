17fce02eb8e0df4c063e9f91530803f4da6a35f7
louisjeon <wjss5115@gmail.com>
2024-12-01 00:00:39 +0900

    fix cors

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["127.0.0.1:3000"],
     optionsSuccessStatus: 200,
     credentials: true,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
