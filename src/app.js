c71f3c7656812ec4eb68ff2f07f9ef2cfe09076c
louisjeon <wjss5115@gmail.com>
2024-12-01 00:13:57 +0900

    a

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: ["127.0.0.1:3000"],
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "OPTIONS"],
   })
 );
