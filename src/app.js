9550606005fff2c3203144a0623390a4a540c82f
louisjeon <wjss5115@gmail.com>
2024-11-30 23:39:06 +0900

    change cors config

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000"],
     optionsSuccessStatus: 200,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     preflightContinue: false,
