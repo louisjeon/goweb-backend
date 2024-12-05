9f85852bff6beaaf9be4b3e592577bdd90dc8c62
louisjeon <wjss5115@gmail.com>
2024-12-05 23:56:27 +0900

    update CORS

--- src/app.js
+++ src/app.js
@@ -13,11 +13,9 @@ const commentRoutes = require("./routes/comment.routes"); // 댓글 라우트
 
 const app = express();
 
-//["http://localhost:3000", "https://goweb-front.vercel.app"]
-
 // CORS 설정
 const corsOptions = {
-  origin: "*", // 허용할 출처
+  origin: ["http://localhost:3000", "https://goweb-front.vercel.app"], // 허용할 출처
   methods: "GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS", // 허용할 HTTP 메서드
   allowedHeaders: "Content-Type,Authorization", // 허용할 헤더
 };
