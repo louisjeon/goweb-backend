6e3002afb786582cc7d96a4964848a1237746b6a
louisjeon <wjss5115@gmail.com>
2024-12-06 22:24:04 +0900

    revert CORS trial

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -4,24 +4,6 @@ const router = express.Router();
 const postController = require("../controllers/post.controller");
 
 // 게시글 CRUD 라우트
-const cors = require("cors");
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://goweb-front.vercel.app",
-]; // 허용할 출처
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: "GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS", // 허용할 HTTP 메서드
-  allowedHeaders: "Content-Type,Authorization", // 허용할 헤더
-};
-router.use(cors());
-router.options("*", cors(corsOptions));
 router.post("/", postController.createPost); // 게시글 생성
 router.get("/", postController.getAllPosts); // 모든 게시글 조회
 router.get("/:id", postController.getPostById); // 특정 게시글 조회
